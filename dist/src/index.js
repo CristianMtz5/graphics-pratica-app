11a3178170505424eab1ce4608c2d348642084cf
Cristian Ivan M. P <maper423@gmail.com>
2022-06-28 20:48:37 -0500

    Commit en Proyecto

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,13 +1,13 @@
 //import { Input } from './Input.js';
-import { Obj3D } from "./Obj3D.js";
+import { Obj3D } from './Obj3D.js';
 //import { Canvas3D } from './Canvas3D.js';
 //import { CvWireframe } from './CvWireFrame.js';
-import { CvHLines } from "./CvHLines.js";
-import { Rota3D } from "./Rota3D.js";
+import { CvHLines } from './CvHLines.js';
+import { Rota3D } from './Rota3D.js';
 let canvas;
 let graphics;
-canvas = document.getElementById("circlechart");
-graphics = canvas.getContext("2d");
+canvas = document.getElementById('circlechart');
+graphics = canvas.getContext('2d');
 let cv;
 let obj;
 let ang = 0;
@@ -31,20 +31,19 @@ function leerArchivo(e) {
     lector.readAsText(archivo);
 }
 function mostrarContenido(contenido) {
-    var elemento = document.getElementById("contenido-archivo");
+    var elemento = document.getElementById('contenido-archivo');
     //
     //readObject(new Input(contenido));
     elemento.innerHTML = contenido;
 }
 function vp(dTheta, dPhi, fRho) {
-    // Viewpoint
     if (obj != undefined) {
         let obj = cv.getObj();
         if (!obj.vp(cv, dTheta, dPhi, fRho))
-            alert("datos no validos");
+            alert('datos no validos');
     }
     else
-        alert("aun no has leido un archivo");
+        alert('aun no has leido un archivo');
 }
 function eyeDownFunc() {
     vp(0, 0.1, 1);
@@ -66,16 +65,7 @@ function decrDistFunc() {
 }
 function pza1DerFunc() {
     let af = 10;
-    Rota3D.initRotate(obj.w[139], obj.w[140], (af * Math.PI) / 180);
-    for (let i = 201; i <= 238; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
-    }
-    cv.setObj(obj);
-    cv.paint();
-}
-function pza1IzqFunc() {
-    let af = -10;
-    Rota3D.initRotate(obj.w[139], obj.w[140], (af * Math.PI) / 180);
+    Rota3D.initRotate(obj.w[139], obj.w[140], af * Math.PI / 180);
     for (let i = 201; i <= 238; i++) {
         obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
@@ -85,7 +75,7 @@ function pza1IzqFunc() {
 function pza12DerFunc() {
     let af = 10;
     console.log(obj.w[29], obj.w[30], obj.w[6]);
-    Rota3D.initRotate(obj.w[29], obj.w[30], (af * Math.PI) / 180);
+    Rota3D.initRotate(obj.w[29], obj.w[30], af * Math.PI / 180);
     for (let i = 101; i <= 140; i++) {
         obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
@@ -98,7 +88,7 @@ function pza12DerFunc() {
 function pza12IzqFunc() {
     let af = -10;
     console.log(obj.w[29], obj.w[30]);
-    Rota3D.initRotate(obj.w[29], obj.w[30], (af * Math.PI) / 180);
+    Rota3D.initRotate(obj.w[29], obj.w[30], af * Math.PI / 180);
     for (let i = 101; i <= 140; i++) {
         obj.w[i] = Rota3D.rotate(obj.w[i]);
     }
@@ -108,38 +98,33 @@ function pza12IzqFunc() {
     cv.setObj(obj);
     cv.paint();
 }
-document
-    .getElementById("file-input")
-    .addEventListener("change", leerArchivo, false);
-document
-    .getElementById("eyeDown")
-    .addEventListener("click", eyeDownFunc, false);
-document.getElementById("eyeUp").addEventListener("click", eyeUpFunc, false);
-document
-    .getElementById("eyeLeft")
-    .addEventListener("click", eyeLeftFunc, false);
-document
-    .getElementById("eyeRight")
-    .addEventListener("click", eyeRightFunc, false);
-document
-    .getElementById("incrDist")
-    .addEventListener("click", incrDistFunc, false);
-document
-    .getElementById("decrDist")
-    .addEventListener("click", decrDistFunc, false);
+function girarHelices() {
+    let af = 10;
+    Rota3D.initRotate(obj.w[170], obj.w[214], (af * Math.PI) / 180);
+    for (let i = 151; i <= 214; i++) {
+        obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+    cv.setObj(obj);
+    cv.paint();
+}
+let intervaloH;
+function pza1Der() {
+    intervaloH = setInterval(girarHelices, 100);
+}
+function pza12Der() {
+    clearInterval(intervaloH);
+}
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
+document.getElementById('eyeUp').addEventListener('click', eyeUpFunc, false);
+document.getElementById('eyeLeft').addEventListener('click', eyeLeftFunc, false);
+document.getElementById('eyeRight').addEventListener('click', eyeRightFunc, false);
+document.getElementById('incrDist').addEventListener('click', incrDistFunc, false);
+document.getElementById('decrDist').addEventListener('click', decrDistFunc, false);
 //movimiento de piezas
-document
-    .getElementById("pza1Izq")
-    .addEventListener("click", pza1IzqFunc, false);
-document
-    .getElementById("pza1Der")
-    .addEventListener("click", pza1DerFunc, false);
-document
-    .getElementById("pza12Izq")
-    .addEventListener("click", pza12IzqFunc, false);
-document
-    .getElementById("pza12Der")
-    .addEventListener("click", pza12DerFunc, false);
+document.getElementById('pza1Izq').addEventListener('click', girarHelices, false);
+document.getElementById('pza1Der').addEventListener('click', pza1Der, false);
+document.getElementById('pza12Der').addEventListener('click', pza12Der, false);
 let Pix, Piy;
 let Pfx, Pfy;
 let theta = 0.3, phi = 1.3, SensibilidadX = 0.02, SensibilidadY = 0.02;
@@ -156,7 +141,7 @@ function makeVizualization(evento) {
         //console.log(Pfx, Pfy)
         let difX = Pix - Pfx;
         let difY = Pfy - Piy;
-        vp(0, (0.1 * difY) / 50, 1);
+        vp(0, 0.1 * difY / 50, 1);
         Piy = Pfy;
         vp(0.1 * difX, 0 / 50, 1);
         Pix = Pfx;
@@ -191,6 +176,9 @@ function makeVizualization(evento) {
 function noDraw() {
     flag = false;
 }
-canvas.addEventListener("mousedown", handleMouse);
-canvas.addEventListener("mouseup", noDraw);
-canvas.addEventListener("mousemove", makeVizualization);
+canvas.addEventListener('mousedown', handleMouse);
+canvas.addEventListener('mouseup', noDraw);
+canvas.addEventListener('mousemove', makeVizualization);
+function timeout(arg0, timeout) {
+    throw new Error('Function not implemented.');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 //import { Input } from './Input.js';
-import { Obj3D } from "./Obj3D.js";
+import { Obj3D } from './Obj3D.js';
 //import { Canvas3D } from './Canvas3D.js';
 //import { CvWireframe } from './CvWireFrame.js';
-import { CvHLines } from "./CvHLines.js";
-import { Rota3D } from "./Rota3D.js";
-import { Point3D } from "./point3D.js";
+import { CvHLines } from './CvHLines.js';
+import { Rota3D } from './Rota3D.js';
+import { Point3D } from './point3D.js';
 
 let canvas: HTMLCanvasElement;
 let graphics: CanvasRenderingContext2D;
 
-canvas = <HTMLCanvasElement>document.getElementById("circlechart");
-graphics = canvas.getContext("2d");
+canvas = <HTMLCanvasElement>document.getElementById('circlechart');
+graphics = canvas.getContext('2d');
 
 let cv: CvHLines;
 let obj: Obj3D;
-let ang: number = 0;
+let ang: number=0;
 
-function leerArchivo(e: any) {
+function leerArchivo(e:any) {
   var archivo = e.target.files[0];
   if (!archivo) {
     return;
   }
   var lector = new FileReader();
-  lector.onload = function (e) {
+  lector.onload = function(e) {
     var contenido = e.target.result;
     mostrarContenido(contenido);
     obj = new Obj3D();
@@ -36,19 +36,21 @@ function leerArchivo(e: any) {
   lector.readAsText(archivo);
 }
 
-function mostrarContenido(contenido: any) {
-  var elemento = document.getElementById("contenido-archivo");
+function mostrarContenido(contenido:any) {
+  var elemento = document.getElementById('contenido-archivo');
   //
   //readObject(new Input(contenido));
   elemento.innerHTML = contenido;
 }
 
-function vp(dTheta: number, dPhi: number, fRho: number): void {
-  // Viewpoint
+function vp(dTheta:number, dPhi:number, fRho:number):void{  // Viewpoint
   if (obj != undefined) {
     let obj: Obj3D = cv.getObj();
-    if (!obj.vp(cv, dTheta, dPhi, fRho)) alert("datos no validos");
-  } else alert("aun no has leido un archivo");
+    if (!obj.vp(cv, dTheta, dPhi, fRho))
+      alert('datos no validos');
+  }
+  else
+    alert('aun no has leido un archivo');
 }
 
 function eyeDownFunc() {
@@ -77,102 +79,88 @@ function decrDistFunc() {
 
 function pza1DerFunc() {
   let af = 10;
-
-  Rota3D.initRotate(obj.w[139], obj.w[140], (af * Math.PI) / 180);
-
-  for (let i = 201; i <= 238; i++) {
+ 	
+	Rota3D.initRotate( obj.w[139], obj.w[140], af*Math.PI/180);	
+	
+  for (let i = 201; i <= 238; i++){
     obj.w[i] = Rota3D.rotate(obj.w[i]);
-  }
-  cv.setObj(obj);
-  cv.paint();
+	}
+	cv.setObj(obj);
+  cv.paint();	
 }
 
-function pza1IzqFunc() {
-  let af = -10;
-
-  Rota3D.initRotate(obj.w[139], obj.w[140], (af * Math.PI) / 180);
-
-  for (let i = 201; i <= 238; i++) {
-    obj.w[i] = Rota3D.rotate(obj.w[i]);
-  }
-  cv.setObj(obj);
-  cv.paint();
-}
 function pza12DerFunc() {
   let af = 10;
   console.log(obj.w[29], obj.w[30], obj.w[6]);
-  Rota3D.initRotate(obj.w[29], obj.w[30], (af * Math.PI) / 180);
-
-  for (let i = 101; i <= 140; i++) {
+  Rota3D.initRotate(obj.w[29], obj.w[30], af * Math.PI / 180);
+	
+  for (let i = 101; i <= 140; i++){
     obj.w[i] = Rota3D.rotate(obj.w[i]);
   }
-  for (let i = 201; i <= 238; i++) {
+  for (let i = 201; i <= 238; i++){
     obj.w[i] = Rota3D.rotate(obj.w[i]);
-  }
-  cv.setObj(obj);
-  cv.paint();
+	}
+	cv.setObj(obj);
+  cv.paint();	
 }
 
 function pza12IzqFunc() {
   let af = -10;
   console.log(obj.w[29], obj.w[30]);
-  Rota3D.initRotate(obj.w[29], obj.w[30], (af * Math.PI) / 180);
+	Rota3D.initRotate( obj.w[29], obj.w[30], af*Math.PI/180);	
+	
+  for (let i = 101; i <= 140; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+	}
+  for (let i = 201; i <= 238; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+	}
+  
+	cv.setObj(obj);
+  cv.paint();	
+}
 
-  for (let i = 101; i <= 140; i++) {
+function girarHelices() {
+ let af = 10;
+
+  Rota3D.initRotate(obj.w[170], obj.w[214], (af * Math.PI) / 180);
+  for (let i = 151; i <=214; i++) {
     obj.w[i] = Rota3D.rotate(obj.w[i]);
   }
-  for (let i = 201; i <= 238; i++) {
-    obj.w[i] = Rota3D.rotate(obj.w[i]);
-  }
-
   cv.setObj(obj);
   cv.paint();
 }
 
-document
-  .getElementById("file-input")
-  .addEventListener("change", leerArchivo, false);
-document
-  .getElementById("eyeDown")
-  .addEventListener("click", eyeDownFunc, false);
-document.getElementById("eyeUp").addEventListener("click", eyeUpFunc, false);
-document
-  .getElementById("eyeLeft")
-  .addEventListener("click", eyeLeftFunc, false);
-document
-  .getElementById("eyeRight")
-  .addEventListener("click", eyeRightFunc, false);
-document
-  .getElementById("incrDist")
-  .addEventListener("click", incrDistFunc, false);
-document
-  .getElementById("decrDist")
-  .addEventListener("click", decrDistFunc, false);
+let intervaloH;
+
+function pza1Der(){
+   intervaloH = setInterval(girarHelices,100);
+}
+
+function pza12Der(){
+  clearInterval(intervaloH);
+}
+
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
+document.getElementById('eyeUp').addEventListener('click', eyeUpFunc, false);
+document.getElementById('eyeLeft').addEventListener('click', eyeLeftFunc, false);
+document.getElementById('eyeRight').addEventListener('click', eyeRightFunc, false);
+document.getElementById('incrDist').addEventListener('click', incrDistFunc, false);
+document.getElementById('decrDist').addEventListener('click', decrDistFunc, false);
 
 //movimiento de piezas
-document
-  .getElementById("pza1Izq")
-  .addEventListener("click", pza1IzqFunc, false);
-document
-  .getElementById("pza1Der")
-  .addEventListener("click", pza1DerFunc, false);
-document
-  .getElementById("pza12Izq")
-  .addEventListener("click", pza12IzqFunc, false);
-document
-  .getElementById("pza12Der")
-  .addEventListener("click", pza12DerFunc, false);
+document.getElementById('pza1Izq').addEventListener('click', girarHelices, false);
+document.getElementById('pza1Der').addEventListener('click', pza1Der, false);
+document.getElementById('pza12Der').addEventListener('click', pza12Der, false);
 
 let Pix: number, Piy: number;
 let Pfx: number, Pfy: number;
-let theta = 0.3,
-  phi = 1.3,
-  SensibilidadX = 0.02,
-  SensibilidadY = 0.02;
+let theta = 0.3, phi = 1.3, SensibilidadX = 0.02, SensibilidadY = 0.02;
 let flag: boolean = false;
 
 function handleMouse(evento: any) {
-  Pix = evento.offsetX;
+  Pix=evento.offsetX;
   Piy = evento.offsetY;
   flag = true;
 }
@@ -184,7 +172,7 @@ function makeVizualization(evento: any) {
     //console.log(Pfx, Pfy)
     let difX = Pix - Pfx;
     let difY = Pfy - Piy;
-    vp(0, (0.1 * difY) / 50, 1);
+    vp(0, 0.1 * difY / 50, 1);
     Piy = Pfy;
     vp(0.1 * difX, 0 / 50, 1);
     Pix = Pfx;
@@ -222,6 +210,10 @@ function noDraw() {
   flag = false;
 }
 
-canvas.addEventListener("mousedown", handleMouse);
-canvas.addEventListener("mouseup", noDraw);
-canvas.addEventListener("mousemove", makeVizualization);
+canvas.addEventListener('mousedown', handleMouse);
+canvas.addEventListener('mouseup', noDraw);
+canvas.addEventListener('mousemove', makeVizualization);
+
+function timeout(arg0: () => void, timeout: any) {
+  throw new Error('Function not implemented.');
+}
